a71c652cc1033abce634dd96b43626148fc230aa
thanh6886 <phanquangthanh136@gmail.com>
2023-09-13 14:38:21 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,29 @@
 import React from "react";
 
-const arr = [
-      {
-        a:1
-      },
-      {
-        a:2
-      },
-      {
-        a:3
-      }
-]
+
+
+const songsApi = "https://thanh6886.github.io/usersAPI/usersAPI.json"
 
 
 
-class Car extends React.Component{
-
-  
-      render(){
-        const html =   arr.map((e)=>{
-          return( 
-            <div  className="render">
-                <h2>{e.a}</h2>
-            </div>)
-        })
-
-        return( 
-                         
-          <div>
-             <h1>hello</h1>
-              { html} 
-          </div>
-          )
-      }
+function getApi(callback){
+      fetch(songsApi)
+      .then((data)=>{
+          return data.json()
+      })
+      .then((data)=>{
+            return data.songs
+      })
+      .then(callback)
 }
+  getApi((data)=>{
+      console.log(data)
+  })
 
 
 
-export default Car;
+
+
+export default getApi;
+
+
