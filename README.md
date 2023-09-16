5e8be3201feca5d60020c7862d7d21af4780d445
thanh6886 <phanquangthanh136@gmail.com>
2023-09-17 00:04:52 +0700

    .

--- README.md
+++ README.md
@@ -44,3 +44,73 @@ class Car extends React.Component {
     );
   }
 }
+
+react HOOK
+
+
+"""""""""""useState *******
+
+const [state, setState] = useState(initSate)
+
+
+// const songsApi = "https://thanh6886.github.io/usersAPI/usersAPI.json";
+
+// function App() {
+//   const [courses, setCourses] = useState([]);
+//       console.log(courses)
+
+//   useEffect(() => {
+//     fetchCourses();
+//   }, []);
+
+//   const fetchCourses = () => {
+//     fetch(songsApi)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setCourses(data.songs);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       {courses.map((element, index) => (
+//         <PostItem
+//           key={index}
+//           name={element.name}
+//           singer={element.singer}
+//           image={element.image}
+//           song={element.path}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function PostItem(props) {
+//   return (
+//     <div>
+//       <div className="listsong">
+//         <div className="song">
+//         <p><a href={props.song}></a> click</p>
+//           <h1>{props.name}</h1>
+//           <h2>{props.singer}</h2>
+//           <div>
+//             <img src={props.image} alt={`${props.name} cover`} />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+      const [counter, setCounter] = useState({
+            name:"thanh",
+            age: 21
+      })
+      const handleClick = ()=>{    
+            setCounter((e) => ({
+                  ...e,
+                  address:'hello'   
+            }))
+      }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,75 +1,36 @@
-import React, { useState, useEffect } from "react";
-
-// const songsApi = "https://thanh6886.github.io/usersAPI/usersAPI.json";
-
-// function App() {
-//   const [courses, setCourses] = useState([]);
-//       console.log(courses)
-
-//   useEffect(() => {
-//     fetchCourses();
-//   }, []);
-
-//   const fetchCourses = () => {
-//     fetch(songsApi)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setCourses(data.songs);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       {courses.map((element, index) => (
-//         <PostItem
-//           key={index}
-//           name={element.name}
-//           singer={element.singer}
-//           image={element.image}
-//           song={element.path}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// function PostItem(props) {
-//   return (
-//     <div>
-//       <div className="listsong">
-//         <div className="song">
-//         <p><a href={props.song}></a> click</p>
-//           <h1>{props.name}</h1>
-//           <h2>{props.singer}</h2>
-//           <div>
-//             <img src={props.image} alt={`${props.name} cover`} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import React  , { useState, useEffect } from "react";
 
 
-const handClick = ()=>{
-            console.log("hello")
-}
-
-function Iteam({Click}){
+function Button({tilte, click}){
             return(
-                  <h1 onClick={Click}> cick me</h1>
+                  <button onClick={click}>{tilte}</button>
             )
 }
 
+
+function index(){
+      return 100
+}
+
+
+
 function App(){
+      const [counter, setCounter] = useState(index)
+      console.log(counter)
+      const handleClick = ()=>{    
+            setCounter(counter + 1)
+      }
       return(
-            <div id="demo">
-                  <Iteam
-                        Click={handClick}
-                  
-                  />                  
-           </div>
+            <div>
+                  <h1>{counter}</h1>
+                  <Button
+                  tilte="click me"
+                  click={handleClick}
+                  />
+            </div>
       )
 }
+
+
 
 export default App;
