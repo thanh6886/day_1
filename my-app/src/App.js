94228e643f0fa8e6df08ac2004403d5449e7137b
thanh6886 <phanquangthanh136@gmail.com>
2023-09-16 20:53:52 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// const songsApi = "https://thanh6886.github.io/usersAPI/usersAPI.json";
+
+// function App() {
+//   const [courses, setCourses] = useState([]);
+//       console.log(courses)
+
+//   useEffect(() => {
+//     fetchCourses();
+//   }, []);
+
+//   const fetchCourses = () => {
+//     fetch(songsApi)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setCourses(data.songs);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       {courses.map((element, index) => (
+//         <PostItem
+//           key={index}
+//           name={element.name}
+//           singer={element.singer}
+//           image={element.image}
+//           song={element.path}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function PostItem(props) {
+//   return (
+//     <div>
+//       <div className="listsong">
+//         <div className="song">
+//         <p><a href={props.song}></a> click</p>
+//           <h1>{props.name}</h1>
+//           <h2>{props.singer}</h2>
+//           <div>
+//             <img src={props.image} alt={`${props.name} cover`} />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 
+const handClick = ()=>{
+            console.log("hello")
+}
 
-const songsApi = "https://thanh6886.github.io/usersAPI/usersAPI.json"
-
-
-
-function App(){
+function Iteam({Click}){
             return(
-                  <div>
-                        <PostItiem
-                              name="Về Quê"
-                              image="https://toigingiuvedep.vn/wp-content/uploads/2022/11/hinh-anh-gai-xinh-de-thuong-deo-kinh-400x600.jpg"
-                        />
-                  </div>
+                  <h1 onClick={Click}> cick me</h1>
             )
 }
 
-function PostItiem(props){
+function App(){
       return(
-            <div>
-                <div className="listsong">
-                    <div className="song">
-                            <h1>{props.name}</h1>
-                         <div>
-                           <img src={props.image}/>
-                         </div>
-                   </div>
-                </div>
-            </div>
+            <div id="demo">
+                  <Iteam
+                        Click={handClick}
+                  
+                  />                  
+           </div>
       )
 }
 
-
-
-
-
-
-
-
-
 export default App;
-
-
